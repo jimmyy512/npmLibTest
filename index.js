92380575ad0e135dee68012e185b907f8b3cc436
Jim <jimmyy5121@gmail.com>
2019-09-06 18:29:26 +0800

    添加es6 引入方法

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
+//common.js
 var test=require("./lib/test");
 console.log(test.test1());
 console.log(test.test2());
 module.exports={test};
+
+//ES6
+import DrawTableLine from "./lib/DrawLine";
+export default {
+    DrawTableLine
+}
